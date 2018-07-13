6f3379e8b73bf197075b052a951123ef7693c6dc
Huy Trong Nguyen <huynguyen8505@gmail.com>
2018-07-13 19:51:58 +0700

    #1 Create mockup screen for Client Profile service (Login screen)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import 'babel-polyfill';
+import './components/common/common.module';
+
+Ext.application({
+  name: 'MyApp',
+
+  mainView: 'MyApp.Login',
+
+  stores: [
+    // TODO: add global / shared stores here
+  ],
+
+  launch() {
+    // TODO - Launch the application
+  },
+});
